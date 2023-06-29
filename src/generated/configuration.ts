e53dd75ed81c4cee54235932a2d1dab365d70a2c
tsviatkov <nick.tsviatkov@gmail.com>
2023-06-29 08:11:30 +0300

    Update the SDK after switching the api to JSON format

--- src/generated/configuration.ts
+++ src/generated/configuration.ts
@@ -12,15 +12,12 @@
  * Do not edit the class manually.
  */
 
+
 export interface ConfigurationParameters {
     apiKey?: string | Promise<string> | ((name: string) => string) | ((name: string) => Promise<string>);
     username?: string;
     password?: string;
-    accessToken?:
-        | string
-        | Promise<string>
-        | ((name?: string, scopes?: string[]) => string)
-        | ((name?: string, scopes?: string[]) => Promise<string>);
+    accessToken?: string | Promise<string> | ((name?: string, scopes?: string[]) => string) | ((name?: string, scopes?: string[]) => Promise<string>);
     basePath?: string;
     baseOptions?: any;
     formDataCtor?: new () => any;
@@ -53,11 +50,7 @@ export class Configuration {
      * @param scopes oauth2 scope
      * @memberof Configuration
      */
-    accessToken?:
-        | string
-        | Promise<string>
-        | ((name?: string, scopes?: string[]) => string)
-        | ((name?: string, scopes?: string[]) => Promise<string>);
+    accessToken?: string | Promise<string> | ((name?: string, scopes?: string[]) => string) | ((name?: string, scopes?: string[]) => Promise<string>);
     /**
      * override base path
      *
@@ -102,7 +95,7 @@ export class Configuration {
      * @return True if the given MIME is JSON, false otherwise.
      */
     public isJsonMime(mime: string): boolean {
-        const jsonMime: RegExp = new RegExp("^(application/json|[^;/ \t]+/[^;/ \t]+[+]json)[ \t]*(;.*)?$", "i");
-        return mime !== null && (jsonMime.test(mime) || mime.toLowerCase() === "application/json-patch+json");
+        const jsonMime: RegExp = new RegExp('^(application\/json|[^;/ \t]+\/[^;/ \t]+[+]json)[ \t]*(;.*)?$', 'i');
+        return mime !== null && (jsonMime.test(mime) || mime.toLowerCase() === 'application/json-patch+json');
     }
 }
