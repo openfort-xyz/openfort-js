1bfd1d062d83fa0a81d9a1d1e6c9fcf195200c9e
Jaume Alavedra <jamalavedra@gmail.com>
2024-05-17 13:46:18 +0200

    fix: add missing dependency

--- sdk/rollup.config.js
+++ sdk/rollup.config.js
@@ -1,35 +1,23 @@
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
-
+import {readFileSync} from 'fs';
 import commonJs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import dts from 'rollup-plugin-dts';
 import replace from '@rollup/plugin-replace';
 import pkg from './package.json' assert {type: 'json'};
-// import moduleReleases from './module-release.json' assert {type: 'json'};
 
-// RELEASE_TYPE environment variable is set by the CI/CD pipeline
-const releaseType = process.env.RELEASE_TYPE || 'alpha';
+const packages = JSON.parse(
+  readFileSync('./workspace-packages.json', {encoding: 'utf8'})
+);
 
-const getPackages = () =>
-  [
-    {location: 'packages/platform-bridge', name: '@openfort/platform-bridge'},
-    {location: 'sdk', name: '@openfort/openfort-js'},
-  ].map((pkg) => pkg.name);
+const getPackages = () => packages.map((pkg) => pkg.name);
 
 // Get relevant files to bundle
 const getFilesToBuild = () => {
   // Always build the index file
   const files = ['index'];
 
-  const moduleFiles = []; //Object.keys(moduleReleases.modules);
-  if (releaseType === 'alpha') {
-    return [...files, ...moduleFiles];
-  }
-
-  // const returnModules = moduleFiles.filter(
-  //   (file) => moduleReleases.modules[file] === 'prod'
-  // );
   return [...files];
 };
 
