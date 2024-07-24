3e456581c13a08e559576ad9560bd75f8bcde3c5
Jaume Alavedra <jamalavedra@gmail.com>
2024-07-24 19:57:36 +0200

    fix: update sample

--- examples/apps/auth-sample/src/utils/openfortAdminConfig.ts
+++ examples/apps/auth-sample/src/utils/openfortAdminConfig.ts
@@ -1,6 +1,5 @@
 import Openfort from '@openfort/openfort-node';
 
-// Ensure Openfort is only initialized once
 const openfort = (() => {
   if (!process.env.NEXTAUTH_OPENFORT_SECRET_KEY) {
     throw new Error("Openfort secret key is not set");
