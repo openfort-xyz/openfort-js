7b1e8abb79736a56ad6063513cfb55c4e31e3099
Aleix <nigeon@gmail.com>
2024-10-08 21:45:15 +0200

    WalletConnect pairing

--- examples/apps/auth-sample/src/pages/index.tsx
+++ examples/apps/auth-sample/src/pages/index.tsx
@@ -20,6 +20,7 @@ import CreateSessionButton from '../components/Sessions/CreateSessionButton';
 import LinkOAuthButton from '../components/OAuth/LinkOAuthButton';
 import ExportPrivateKey from '../components/Export/ExportPrivateKeyButton';
 import SetWalletRecovery from '../components/SetWalletRecovery/SetWalletRecoveryButton';
+import { SetPairingCodeWithWagmi } from '../components/WalletConnect/SetPairingCode';
 
 const HomePage: NextPage = () => {
   const {state} = useOpenfort();
@@ -93,20 +94,22 @@ const HomePage: NextPage = () => {
   }
 
   return (
-    <main className="flex min-h-full overflow-hidden pt-8 sm:py-14">
-      <div className="mx-auto flex w-full max-w-2xl flex-col px-4 sm:px-6">
-        <div className="flex mb-4">
+    <>
+      <header>
+        <div className="flex mb-4 justify-between p-2">
           <Link href="/" aria-label="Home">
             <Logo className="block h-8 w-auto" />
           </Link>
+
+          <div className="">
+            <LogoutButton />
+          </div>
         </div>
-        <p className="text-gray-400 mb-2">Welcome, {user?.id}!</p>
-        <div className="absolute top-2 right-2">
-          <LogoutButton />
-        </div>
-        <div className="mb-8">
+      </header>
+      <main className="flex min-h-full overflow-hidden px-10">
+        <div className="w-[400px]">
           <span className="font-medium text-black">Console: </span>
-          <div className="py-4 block h-full">
+          <div className="mt-4 block h-full w-full">
             <textarea
               ref={textareaRef}
               className="no-scrollbar h-full w-full rounded-lg border-0 bg-gray-100 p-4 font-mono text-xs text-black"
@@ -115,105 +118,131 @@ const HomePage: NextPage = () => {
             />
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-          <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
-            <h2 className="flex justify-left font-medium text-xl pb-4">
-              Account actions
-            </h2>
-            <div>
-              <span className="font-medium text-black">Backend action: </span>
-              <MintNFTButton handleSetMessage={handleSetMessage} />
-            </div>
-            <div>
-              <span className="font-medium text-black">Ethers provider: </span>
-              <EvmProviderButton handleSetMessage={handleSetMessage} />
-            </div>
-            <div>
-              <button
-                type="button"
-                onClick={() => {
-                  router.push('/link-wallet');
-                }}
-                className={
-                  'mt-4 w-40 px-4 py-2 border-black text-black font-semibold rounded-lg shadow-md hover:bg-gray-100 disabled:bg-gray-400 disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50'
-                }
-              >
-                {'Link a Wallet'}
-              </button>
-            </div>
-          </div>
-          <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
-            <h2 className="flex justify-left font-medium text-xl pb-4">
-              Signatures
-            </h2>
-            <div>
-              <span className="font-medium text-black">Message: </span>Hello
-              World!
-              <SignMessageButton handleSetMessage={handleSetMessage} />
-            </div>
-            <div>
-              <span className="font-medium text-black">Typed message: </span>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://github.com/openfort-xyz/sample-browser-nextjs-embedded-signer/blob/main/src/components/Signatures/SignTypedDataButton.tsx#L25"
-                className="text-blue-600 hover:underline"
-              >
-                {'View typed message.'}
-              </a>
-              <SignTypedDataButton handleSetMessage={handleSetMessage} />
-            </div>
-          </div>
-          <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
-            <h2 className="flex justify-left font-medium text-xl pb-4">
-              Linked socials
-            </h2>
-
-            <div>
-              <span className="font-medium text-black">Get user: </span>
-              <GetUserButton handleSetMessage={handleSetMessage} />
-            </div>
-            <div>
-              <span className="font-medium text-black">{'OAuth methods'}</span>
-              {[
-                OAuthProvider.GOOGLE,
-                OAuthProvider.TWITTER,
-                OAuthProvider.FACEBOOK,
-              ].map((provider) => (
-                <div key={provider}>
-                  <LinkOAuthButton provider={provider} user={user} />
+        <div className="w-full">
+          <div className="mx-auto flex w-full max-w-2xl flex-col px-4 sm:px-6">
+            <p className="text-gray-400 mb-2">Welcome, {user?.id}!</p>
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+              <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
+                <h2 className="flex justify-left font-medium text-xl pb-4">
+                  Account actions
+                </h2>
+                <div>
+                  <span className="font-medium text-black">
+                    Backend action:{' '}
+                  </span>
+                  <MintNFTButton handleSetMessage={handleSetMessage} />
                 </div>
-              ))}
-            </div>
-          </div>
-          <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
-            <h2 className="flex justify-left font-medium text-xl pb-4">
-              Session keys
-            </h2>
-            <div>
-              <CreateSessionButton handleSetMessage={handleSetMessage} />
-            </div>
-          </div>
-          <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
-            <h2 className="flex justify-left font-medium text-xl pb-4">
-              Embedded wallet
-            </h2>
-            <div>
-              <span className="font-medium text-black">
-                Export wallet private key:
-              </span>
-              <ExportPrivateKey handleSetMessage={handleSetMessage} />
-            </div>
-            <div>
-              <p className="font-medium text-black mb-4">
-                Change wallet recovery:
-              </p>
-              <SetWalletRecovery handleSetMessage={handleSetMessage} />
+                <div>
+                  <span className="font-medium text-black">
+                    Ethers provider:{' '}
+                  </span>
+                  <EvmProviderButton handleSetMessage={handleSetMessage} />
+                </div>
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      router.push('/link-wallet');
+                    }}
+                    className={
+                      'mt-4 w-40 px-4 py-2 border-black text-black font-semibold rounded-lg shadow-md hover:bg-gray-100 disabled:bg-gray-400 disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50'
+                    }
+                  >
+                    {'Link a Wallet'}
+                  </button>
+                </div>
+              </div>
+              <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
+                <h2 className="flex justify-left font-medium text-xl pb-4">
+                  Signatures
+                </h2>
+                <div>
+                  <span className="font-medium text-black">Message: </span>Hello
+                  World!
+                  <SignMessageButton handleSetMessage={handleSetMessage} />
+                </div>
+                <div>
+                  <span className="font-medium text-black">
+                    Typed message:{' '}
+                  </span>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/openfort-xyz/sample-browser-nextjs-embedded-signer/blob/main/src/components/Signatures/SignTypedDataButton.tsx#L25"
+                    className="text-blue-600 hover:underline"
+                  >
+                    {'View typed message.'}
+                  </a>
+                  <SignTypedDataButton handleSetMessage={handleSetMessage} />
+                </div>
+              </div>
+              <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
+                <h2 className="flex justify-left font-medium text-xl pb-4">
+                  Linked socials
+                </h2>
+
+                <div>
+                  <span className="font-medium text-black">Get user: </span>
+                  <GetUserButton handleSetMessage={handleSetMessage} />
+                </div>
+                <div>
+                  <span className="font-medium text-black">
+                    {'OAuth methods'}
+                  </span>
+                  {[
+                    OAuthProvider.GOOGLE,
+                    OAuthProvider.TWITTER,
+                    OAuthProvider.FACEBOOK,
+                  ].map((provider) => (
+                    <div key={provider}>
+                      <LinkOAuthButton provider={provider} user={user} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
+                <h2 className="flex justify-left font-medium text-xl pb-4">
+                  Session keys
+                </h2>
+                <div>
+                  <CreateSessionButton handleSetMessage={handleSetMessage} />
+                </div>
+              </div>
+              <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
+                <h2 className="flex justify-left font-medium text-xl pb-4">
+                  Embedded wallet
+                </h2>
+                <div>
+                  <span className="font-medium text-black">
+                    Export wallet private key:
+                  </span>
+                  <ExportPrivateKey handleSetMessage={handleSetMessage} />
+                </div>
+                <div>
+                  <p className="font-medium text-black mb-4">
+                    Change wallet recovery:
+                  </p>
+                  <SetWalletRecovery handleSetMessage={handleSetMessage} />
+                </div>
+              </div>
+              <div className="bg-white p-4 rounded-md shadow-2xl space-y-4">
+                <h2 className="flex justify-left font-medium text-xl pb-4">
+                  Wallet Connect
+                </h2>
+                <div>
+                  <p className="font-medium text-black mb-4">
+                    Connect via WalletConnect:
+                  </p>
+                  <SetPairingCodeWithWagmi
+                    handleSetMessage={handleSetMessage}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
