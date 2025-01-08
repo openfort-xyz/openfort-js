ec05df09ca543f0f095e877bb8c6c9cfeae21242
Martí Mayoral <76410086+martimayoral@users.noreply.github.com>
2025-01-08 12:41:18 +0100

    Account for react strict mode test

--- examples/apps/auth-sample/tests/Logger.ts
+++ examples/apps/auth-sample/tests/Logger.ts
@@ -13,7 +13,18 @@ export class Logger {
         await this.page.waitForSelector('textarea', { timeout: 5000 });
         this.textArea = this.page.locator("textarea").first()
 
-        await expect(this.textArea).toHaveValue(/.*>[\s\S]*>/, { timeout: 30000 })
+        await this.textArea.textContent()
+
+        try {
+            // expect the console to have logged
+            // > Current account address: 0x...
+            // > { player id ... }
+            // when running in dev mode, the console will have logged twice, because it uses react StrictMode
+            // check if twice first, then check if once
+            await expect(this.textArea).toHaveValue(/.*>[\s\S]*>[\s\S]*>[\s\S]*>[\s\S]/, { timeout: 2000 })
+        } catch {
+            await expect(this.textArea).toHaveValue(/.*>[\s\S]*>[\s\S]/, { timeout: 3000 })
+        }
 
         this.logs.push(await this.textArea.inputValue())
     }
@@ -25,7 +36,7 @@ export class Logger {
 
     async waitForNewLogs(options: { pollInterval?: number, timeout?: number } = {}) {
         const { pollInterval = 1000, timeout = 30000 } = options;
-        
+
         if (!this.textArea) {
             throw new Error("Logger not initialized");
         }
