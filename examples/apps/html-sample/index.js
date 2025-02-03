95fdb6a68af957266eb223fbafbfe3e37276e863
Jaume Alavedra <jamalavedra@gmail.com>
2025-02-03 16:15:25 +0100

    chore: update examples

--- examples/apps/html-sample/index.js
+++ examples/apps/html-sample/index.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', async () => {
       publishableKey: 'pk_test_505bc088-905e-5a43-b60b-4c37ed1f887a',
     },
     shieldConfiguration: {
-      shieldPublishableKey: 'a4b75269-65e7-49c4-a600-6b5d9d6eec66',
-      shieldEncryptionKey: '/cC/ElEv1bCHxvbE/UUH+bLIf8nSLZOrxj8TkKChiY4=',
+      shieldPublishableKey: '56ca3248-d8a4-499c-b07b-aa9d50a2b577',
+      shieldEncryptionKey: 'ApIULM96wggoEnYQ5x+TO5KRiYiHZdcNQ7wKtV1dvulp',
     },
   });
 
