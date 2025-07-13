95f8fc5fb95922a7e0763b187590be1a7133d47f
Jaume Alavedra <jamalavedra@gmail.com>
2025-07-13 22:12:18 +0200

    chore: support overrides wagmi

--- examples/apps/wallet-libraries/next-wagmi/src/openfort.ts
+++ examples/apps/wallet-libraries/next-wagmi/src/openfort.ts
@@ -1,5 +1,9 @@
 import { Openfort } from '@openfort/openfort-js';
 
+export const shieldUrl = process.env.NEXT_PUBLIC_SHIELD_URL ?? 'https://shield.openfort.io';
+export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const iframeUrl = process.env.NEXT_PUBLIC_IFRAME_URL;
+
 export const openfortInstance = new Openfort({
     baseConfiguration: {
         publishableKey: process.env.NEXT_PUBLIC_OPENFORT_PUBLIC_KEY!,
@@ -7,5 +11,10 @@ export const openfortInstance = new Openfort({
     shieldConfiguration: {
         debug: true,
         shieldPublishableKey: process.env.NEXT_PUBLIC_SHIELD_API_KEY!
+    },
+    overrides: {
+        shieldUrl: shieldUrl,
+        backendUrl: backendUrl,
+        iframeUrl: iframeUrl,
     },
 });
