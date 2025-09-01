4ec69c64fd9aa1a72551d1f7015cd90f382b491d
Aleix Fabra <aleixfabra@gmail.com>
2025-09-01 19:30:23 +0200

    getElementById() is slightly faster and more direct

--- examples/apps/html-sample/utils.js
+++ examples/apps/html-sample/utils.js
@@ -1,6 +1,6 @@
 // Helper for displaying status messages.
 const addMessage = (message) => {
-  const messagesDiv = document.querySelector('#messages');
+  const messagesDiv = document.getElementById('messages');
   messagesDiv.style.display = 'block';
   messagesDiv.innerHTML += `> ${message}<br>`;
   console.log(`Debug: ${message}`);
