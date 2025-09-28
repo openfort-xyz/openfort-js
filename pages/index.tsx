9461fcfdbcf8983793ff13b4147d0eff57c09e76
0xward <aradeawardana97@gmail.com>
2025-09-29 04:40:17 +0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import WalletConnectButton from "../components/WalletConnectButton";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>WalletConnect Integration</h1>
+      <WalletConnectButton />
+    </div>
+  );
+}
