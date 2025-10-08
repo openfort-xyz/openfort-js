abc37cde0f0543060bfe3c78c0e3beb3d74e0846
Jaume Alavedra <jamalavedra@gmail.com>
2025-10-08 14:02:38 +0200

    update versions and rm unused sample

--- examples/apps/better-auth-sample/frontend/src/lib/auth-client.ts
+++
@@ -1,5 +0,0 @@
-import { createAuthClient } from "better-auth/client"
-
-export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_BACKEND_URL  // Backend URL
-})
--- examples/apps/better-auth-sample/frontend/src/lib/openfortConfig.ts
+++
@@ -1,9 +0,0 @@
-import { Openfort } from '@openfort/openfort-js';
- 
-const openfort = new Openfort({
-  baseConfiguration: {
-    publishableKey: import.meta.env.VITE_OPENFORT_PUBLISHABLE_KEY || '',
-  }
-});
- 
-export default openfort;
