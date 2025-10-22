d21ba5e27ecaa2291658afe2a9c356290b5787a5
Jaume Alavedra <jamalavedra@gmail.com>
2025-10-22 16:49:11 +0200

    chore: update changelog

---
+++ .changeset/heavy-coats-float.md
@@ -0,0 +1,5 @@
+---
+"@openfort/openfort-js": patch
+---
+
+update iframe to use native app identifier
--- sdk/src/wallets/iframeManager.ts
+++ sdk/src/wallets/iframeManager.ts
@@ -580,7 +580,7 @@ export class IframeManager {
       } else {
         debugLog(
           '[HANDSHAKE DEBUG] Connection already initialized ' +
-          `(isInitialized: ${this.isInitialized}, hasConnection: ${!!this.connection})`
+            `(isInitialized: ${this.isInitialized}, hasConnection: ${!!this.connection})`
         )
       }
 
