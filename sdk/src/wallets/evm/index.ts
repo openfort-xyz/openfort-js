074c79c54ec18e292f073cdb8c7edf8ca0123fca
Martí Mayoral <76410086+martimayoral@users.noreply.github.com>
2025-11-03 18:45:31 +0100

    update types export

--- sdk/src/wallets/evm/index.ts
+++ sdk/src/wallets/evm/index.ts
@@ -1,6 +1,5 @@
 // biome-ignore lint/performance/noBarrelFile: EVM wallet barrel file for internal re-exports
 // biome-ignore lint/performance/noReExportAll: Re-exporting EVM provider
 export * from './evmProvider'
-export { RevokePermissionsRequestParams } from './revokeSession'
 // biome-ignore lint/performance/noReExportAll: Re-exporting EVM types
 export * from './types'
