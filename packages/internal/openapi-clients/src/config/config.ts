2c827dc9159b11bfc8494f8b91aebe292aa3a0b4
Jaume Alavedra <jamalavedra@gmail.com>
2026-01-11 18:57:48 +0100

    chore: update endpoint verification

--- packages/internal/openapi-clients/src/config/config.ts
+++ packages/internal/openapi-clients/src/config/config.ts
@@ -26,18 +26,16 @@ export const createConfig = ({
 	const apiConfigOptions: ApiConfigurationParameters = {
 		basePath,
 		accessToken,
-	};
-
-	// Add x-native-app-identifier header if nativeAppIdentifier is provided
-	if (nativeAppIdentifier) {
-		apiConfigOptions.baseOptions = {
+		baseOptions: {
 			headers: {
-				"x-native-app-identifier": nativeAppIdentifier,
-				"Cookie": null,
+				...(nativeAppIdentifier && {
+					"x-native-app-identifier": nativeAppIdentifier,
+				}),
+				Cookie: null,
 			},
 			withCredentials: false,
-		};
-	}
+		},
+	};
 
 	return new APIConfiguration(apiConfigOptions);
 };
