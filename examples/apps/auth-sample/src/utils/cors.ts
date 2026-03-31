f8c56af5bb66d75d47aba44ce1a0aceec348a8dc
Antoni Galí <antonigaligimeno@gmail.com>
2026-03-31 19:45:17 +0200

    fix: added sllowed origin protection

--- examples/apps/auth-sample/src/utils/cors.ts
+++ examples/apps/auth-sample/src/utils/cors.ts
@@ -1,9 +1,19 @@
 import Cors from 'cors'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', process.env.NEXT_PUBLIC_APP_URL].filter(
+  Boolean
+)
+
 const cors = Cors({
   methods: ['GET', 'OPTIONS', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  origin: '*',
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   credentials: true,
 })
 
